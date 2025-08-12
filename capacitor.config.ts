3a9c281a5567c816401689d0ac6f7a790d8dbade
JavierDP15 <javierdp15@gmail.com>
2025-08-12 17:51:58 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Geon_Biblio',
+  webDir: 'www'
+};
+
+export default config;
