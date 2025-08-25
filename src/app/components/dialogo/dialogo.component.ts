ebc59c6acc21c6376aee0af8aac3ff89610757fa
JavierDP15 <javierdp15@gmail.com>
2025-08-25 16:08:46 +0200

    Mejorado como se muestra el componente dialogo

--- src/app/components/dialogo/dialogo.component.ts
+++ src/app/components/dialogo/dialogo.component.ts
@@ -14,9 +14,67 @@ export class DialogoComponent  implements OnInit {
   @Input() tutorial: Tutorial | null = null;
   @Output() cerrar = new EventEmitter<void>();
 
+  paginas: string[] = [];
+  paginaActual = 0;
+
+  textoMostrado = '';
+  velocidad = 30;
+  animando = false;
+
+  private intervalo: any;
+
   constructor() { }
 
-  async ngOnInit() { }
+  async ngOnInit() {
+    if (this.tutorial?.mensaje) {
+      this.paginarTexto(this.tutorial.mensaje, 200);
+      this.mostrarConAnimacion();
+    }
+  }
+
+  private paginarTexto(texto: string, charsPorPagina: number) {
+    this.paginas = [];
+    for (let i = 0; i < texto.length; i += charsPorPagina) {
+      this.paginas.push(texto.substring(i, i +charsPorPagina));
+    }
+  }
+
+  private mostrarConAnimacion() {
+    this.textoMostrado = '';
+    clearInterval(this.intervalo);
+
+    const texto =this.paginas[this.paginaActual];
+    let i = 0;
+
+    this.animando = true;
+
+    this.intervalo = setInterval(() => {
+      if (i < texto.length) {
+        this.textoMostrado += texto[i];
+        i++;
+      } else {
+        clearInterval(this.intervalo);
+        this.animando = false;
+      }
+    }, this.velocidad);
+  }
+
+  siguiente() {
+    console.log(this.animando);
+    if (!this.animando){
+      if (this.paginaActual < this.paginas.length - 1) {
+        console.log('ayuyu');
+        this.paginaActual++;
+        this.mostrarConAnimacion();
+      } else {
+        this.cerrarDialogo();
+      }
+    } else {
+      clearInterval(this.intervalo);
+      this.textoMostrado = this.paginas[this.paginaActual];
+      this.animando = false;
+    }
+  }
 
   cerrarDialogo() {
     this.cerrar.emit();
