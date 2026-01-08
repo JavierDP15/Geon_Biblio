d202c883acaf4e103da2881b199f3a6eb4aa3409
JavierDP15 <javierdp15@gmail.com>
2026-01-08 23:07:50 +0100

    Transición inmortales animada

--- src/app/pages/inmortales/inmortales.page.ts
+++ src/app/pages/inmortales/inmortales.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
@@ -28,6 +28,11 @@ import { Router } from '@angular/router';
   ]
 })
 export class InmortalesPage implements OnInit {
+  @ViewChild('video') video!: ElementRef<HTMLVideoElement>;
+
+  mostrarVideo = false;
+  categoria: string = '';
+  animacion = false;
 
   constructor(
     private musicaService: MusicaService
@@ -37,8 +42,47 @@ export class InmortalesPage implements OnInit {
   ngOnInit() {
   }
 
-  irA(inmortal: string) {
+  irA(categoria: string) {
     this.musicaService.reproducirSonido('assets/audios/sonido_boton.mp3');
-    this.router.navigate(['/inmortales', inmortal]);
+    this.categoria = categoria;
+    this.mostrarVideo = true;
+    setTimeout(() => {
+      this.animacion = true;
+    }, 3500);
+  }
+
+  saltarVideo() {
+    this.mostrarVideo = false;
+    this.resetVideo(this.video.nativeElement);
+    this.animacion = false;
+    this.router.navigate(['/inmortales', this.categoria]);
+  }
+
+  onTimeUpdate() {
+    const video = this.video.nativeElement;
+
+    if (
+      video.duration &&
+      video.currentTime >= video.duration -2 &&
+      !this.animacion
+    ) {
+      this.animacion = true;
+    }
+  }
+
+  onVideoEnded() {
+    this.mostrarVideo = false;
+    this.resetVideo(this.video.nativeElement);
+    setTimeout(() => {
+      this.animacion = false;
+      this.router.navigate(['/inmortales', this.categoria]);
+    }, 1000)
+  }
+
+  private resetVideo(video: HTMLVideoElement) {
+    if(!video) return;
+    video.pause();
+    video.currentTime = 0;
+    video.load();
   }
 }
